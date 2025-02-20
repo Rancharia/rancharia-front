106653876748adffe4f590a7432f1dbba9f0601b
ChristianAtena <christian.silva@atenaagencia.com>
2025-02-20 14:51:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rancharia-front',
+  webDir: 'dist'
+};
+
+export default config;
